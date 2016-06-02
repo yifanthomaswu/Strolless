3d22f3a7a154648ef2c42a4a5d120dab93c21b1c
Yifan Wu <yw8012@ic.ac.uk>
2016-06-02 19:14:57 +0100

    js for stroller and order

--- html/js/pgsql.js
+++ html/js/pgsql.js
@@ -46,16 +46,6 @@ function addAddress(callback, u_id, house_number, street_name, postcode, descrip
   xmlHttp.send(JSON.stringify({resource:resource}));
 }
 
-function getAddressByUser(callback, u_id) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    callback(xmlHttp.responseText);
-  }
-  var filter = "?filter=u_id%3D" + u_id;
-  xmlHttp.open("GET", URL_API + "_table/web_address" + filter + URL_API_KEY, true);
-  xmlHttp.send(null);
-}
-
 function getAddressById(callback, a_id) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() {
@@ -66,7 +56,17 @@ function getAddressById(callback, a_id) {
   xmlHttp.send(null);
 }
 
-function getRestaurant(callback, r_id) {
+function getAddressByUser(callback, u_id) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var filter = "?filter=u_id%3D" + u_id;
+  xmlHttp.open("GET", URL_API + "_table/web_address" + filter + URL_API_KEY, true);
+  xmlHttp.send(null);
+}
+
+function getRestaurantById(callback, r_id) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() {
     callback(xmlHttp.responseText);
@@ -111,5 +111,95 @@ function getFavouriteByRestaurant(callback, r_id) {
   }
   var filter = "?filter=r_id%3D" + r_id;
   xmlHttp.open("GET", URL_API + "_table/web_favourite" + filter + URL_API_KEY, true);
+  xmlHttp.send(null);
+}
+
+function addStroller(callback, u_id, a_id, r_id, deadline, pickup) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var resource = {u_id:u_id, a_id:a_id, r_id:r_id, deadline:deadline.toJSON(), pickup:pickup.toJSON()};
+  xmlHttp.open("POST", URL_API + "_table/web_stroller?" + URL_API_KEY, true);
+  xmlHttp.send(JSON.stringify({resource:resource}));
+}
+
+function setStrollerReady(callback, s_id) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var resource = {ready:true};
+  xmlHttp.open("PUT", URL_API + "_table/web_stroller?ids=" + s_id + URL_API_KEY, true);
+  xmlHttp.send(JSON.stringify({resource:resource}));
+}
+
+function getStrollerById(callback, s_id) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var filter = "?filter=s_id%3D" + s_id;
+  xmlHttp.open("GET", URL_API + "_table/web_stroller" + filter + URL_API_KEY, true);
+  xmlHttp.send(null);
+}
+
+function getStrollerByUser(callback, u_id) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var filter = "?filter=u_id%3D" + u_id;
+  xmlHttp.open("GET", URL_API + "_table/web_stroller" + filter + URL_API_KEY, true);
+  xmlHttp.send(null);
+}
+
+function getStrollerByRestaurant(callback, r_id) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var filter = "?filter=r_id%3D" + r_id;
+  xmlHttp.open("GET", URL_API + "_table/web_stroller" + filter + URL_API_KEY, true);
+  xmlHttp.send(null);
+}
+
+function addOrder(callback, u_id, s_id, food) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var resource = {u_id:u_id, s_id:s_id, food:food};
+  xmlHttp.open("POST", URL_API + "_table/web_order?" + URL_API_KEY, true);
+  xmlHttp.send(JSON.stringify({resource:resource}));
+}
+
+function getOrderById(callback, u_id, s_id) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var filter = "?filter=(u_id%3D" + u_id + ")AND(s_id%3D" + s_id+ ")";
+  xmlHttp.open("GET", URL_API + "_table/web_order" + filter + URL_API_KEY, true);
+  xmlHttp.send(null);
+}
+
+function getOrderByUser(callback, u_id) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var filter = "?filter=u_id%3D" + u_id;
+  xmlHttp.open("GET", URL_API + "_table/web_order" + filter + URL_API_KEY, true);
+  xmlHttp.send(null);
+}
+
+function getOrderByStroller(callback, s_id) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    callback(xmlHttp.responseText);
+  }
+  var filter = "?filter=s_id%3D" + s_id;
+  xmlHttp.open("GET", URL_API + "_table/web_order" + filter + URL_API_KEY, true);
   xmlHttp.send(null);
 }
