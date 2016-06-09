3b417806adfd4632b6500262bc90495df92e0698
Yifan Wu <yw8012@ic.ac.uk>
2016-06-09 13:58:12 +0100

    change api url

--- html/js/pgsql.js
+++ html/js/pgsql.js
@@ -1,4 +1,4 @@
-var URL_API = "https://146.169.45.96:444/api/v2/g1527136_u/";
+var URL_API = "http://146.169.45.96/api/v2/g1527136_u/";
 var URL_API_KEY = "&api_key=7627afb9f57cc676069ff7970f9d9c5597b11ca5994a1bd8e6d1ac13245bb36c";
 var HTTP_OK = 200;
 
