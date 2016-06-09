0bbd47c2c91cd1536596ccae64e89af3c8dd62fe
Bjarne <bp2214@ic.ac.uk>
2016-06-09 14:36:04 +0100

    new approach to duration calculation using the Google Maps API distance matrix

--- html/js/mapSource.js
+++ html/js/mapSource.js
@@ -2,53 +2,64 @@ var map;
 var directionsService;
 
 /*
-* Takes two postcodes and a callback-function
-*
-* calls callback-function with duration (sec) to get from
-* first to second postcode bz foot.
-*/
+ * Takes two postcodes and a callback-function
+ *
+ * calls callback-function with duration (sec) to get from
+ * first to second postcode bz foot.
+ */
 
 function duration(postCode1, postCode2, callback) {
-  var directionsRequest = {
-    origin: postCode1,
-    destination: postCode2,
-    travelMode: google.maps.TravelMode.WALKING,
-    region: "GB"
-  };
-  directionsService.route(directionsRequest, function(result, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      dur = (result.routes[0].legs[0].duration.value);
-      callback(dur);
-    } else {
-      callback(-1);
-    }
-  });
+    var directionsRequest = {
+        origin: postCode1,
+        destination: postCode2,
+        travelMode: google.maps.TravelMode.WALKING,
+        region: "GB"
+    };
+    directionsService.route(directionsRequest, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            dur = (result.routes[0].legs[0].duration.value);
+            callback(dur);
+        } else {
+            callback(-1);
+        }
+    });
+}
+
+function duration2(origin, destinations, callback) {
+    var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix({
+        origins: [origin],
+        destinations: destinations,
+        travelMode: google.maps.TravelMode.WALKING,
+        region: "GB",
+        unitSystem: UnitSystem,
+    }, callback);
 }
 
 function showRoute(postCode1, postCode2, map) {
-  var directionsService = new google.maps.DirectionsService();
-  var directionsRequest = {
-    origin: postCode1,
-    destination: postCode2,
-    travelMode: google.maps.TravelMode.WALKING,
-    region: "GB"
-  };
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  directionsDisplay.setMap(map);
-  directionsService.route(directionsRequest, function(result, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(result);
-    }
-  });
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRequest = {
+        origin: postCode1,
+        destination: postCode2,
+        travelMode: google.maps.TravelMode.WALKING,
+        region: "GB"
+    };
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
+    directionsService.route(directionsRequest, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+        }
+    });
 }
 
 function init() {
-  /*var mapDiv = document.getElementById('map');
-  map = new google.maps.Map(mapDiv, {
-    center: {lat: 51.498672, lng: -0.179381},
-    zoom: 15
-  });*/
-  directionsService = new google.maps.DirectionsService();
+    /*var mapDiv = document.getElementById('map');
+    map = new google.maps.Map(mapDiv, {
+      center: {lat: 51.498672, lng: -0.179381},
+      zoom: 15
+    });*/
+    directionsService = new google.maps.DirectionsService();
 }
 
 
